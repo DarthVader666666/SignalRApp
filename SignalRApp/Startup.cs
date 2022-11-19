04771b8f5a3973d97ea8ad17f4af48bae60668bf
Tara Routray <dev@tararoutray.com>
2022-11-19 16:06:24 +0300

    Added login form.

--- SignalRApp/Startup.cs
+++ SignalRApp/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace SignalRApp
@@ -13,14 +14,13 @@ namespace SignalRApp
         public void Configure(IApplicationBuilder app)
         {
             app.UseDeveloperExceptionPage();
-
             app.UseDefaultFiles();
             app.UseStaticFiles();
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
-            {
+            {            
+                //endpoints.Map("/", async (context) => await context.Response.WriteAsync("Hello I'm Vadim!"));
                 endpoints.MapHub<ChatHub>("/chat");
             });
         }
