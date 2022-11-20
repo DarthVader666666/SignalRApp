5407f755ea8e86c5e6e3fde3ea1c5a125b7f681b
Tara Routray <dev@tararoutray.com>
2022-11-20 13:58:54 +0300

    Used User object in request.

--- SignalRApp/ChatHub.cs
+++ SignalRApp/ChatHub.cs
@@ -7,37 +7,17 @@ namespace SignalRApp
 {
     public class ChatHub : Hub
     {
-        //public async Task Send(string product)
-        //{ 
-        //    await this.Clients.Caller.SendAsync("Notify", "Your product added");
-        //    await this.Clients.All.SendAsync("Receive", $"Added: {product} at {DateTime.Now.ToShortTimeString()}");
-        //}
+        public async Task Send(User user)
+        {
+            user.Age += 5;
+            await this.Clients.All.SendAsync("Receive", user);
+        }
+    }
 
-        //public override async Task OnConnectedAsync()
-        //{
-        //    var context = this.Context.GetHttpContext();
+    public class User
+    { 
+        public string Name { get; set; }    
 
-        //    if (context.Request.Cookies.ContainsKey("name"))
-        //    {
-        //        string userName;
-
-        //        if (context.Request.Cookies.TryGetValue("name", out userName))
-        //        {
-        //            Debug.WriteLine($"name = {userName}");
-        //        }
-        //    }
-
-        //    Debug.WriteLine($"UserAgent = {context.Request.Headers["User-Agent"]}");
-        //    Debug.WriteLine($"RemoteIpAddress = {context.Connection.RemoteIpAddress.ToString()}");
-
-        //    await this.Clients.All.SendAsync("Notify", $"{this.Context.ConnectionId} entered chat");
-        //    await base.OnConnectedAsync();
-        //}
-
-        //public override async Task OnDisconnectedAsync(Exception exception)
-        //{
-        //    await this.Clients.All.SendAsync("Notify", $"{this.Context.ConnectionId} left chat");
-        //    await base.OnDisconnectedAsync(exception);
-        //}
+        public int Age { get; set; }
     }
 }
